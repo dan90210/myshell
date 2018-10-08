df367eb736fa0826260d4c42ff6a21210a625be2
dan90210 <hessler.dan@gmail.com>
2018-10-08 08:36:58 -0500

    gave it tabs and stuff

--- myshell.c
+++ myshell.c
@@ -1,6 +1,6 @@
 /*
  * This code implements a simple shell program
- * It supports the internal shell command "exit", 
+ * It supports the internal shell command "exit",
  * backgrounding processes with "&", input redirection
  * with "<" and output redirection with ">".
  * However, this is not complete.
@@ -18,251 +18,249 @@ extern char **getaline();
  * Handle exit signals from child processes
  */
 void sig_handler(int signal) {
-  int status;
-  int result = wait(&status);
+        int status;
+        int result = wait(&status);
 
-  printf("Wait returned %d\n", result);
+        printf("Wait returned %d\n", result);
 }
- 
+
 /*
  * The main shell function
- */ 
+ */
 main() {
-  int i;
-  char **args; 
-  int result;
-  int block;
-  int output;
-  int input;
-  char *output_filename;
-  char *input_filename;
+        int i;
+        char **args;
+        int result;
+        int block;
+        int output;
+        int input;
+        char *output_filename;
+        char *input_filename;
 
-  // Set up the signal handler
-  sigset(SIGCHLD, sig_handler);
+        // Set up the signal handler
+        sigset(SIGCHLD, sig_handler);
 
-  // Loop forever
-  while(1) {
+        // Loop forever
+        while(1) {
 
-    // Print out the prompt and get the input
-    printf("->");
-    args = getaline();
+                // Print out the prompt and get the input
+                printf("->");
+                args = getaline();
 
-    // No input, continue
-    if(args[0] == NULL)
-      continue;
+                // No input, continue
+                if(args[0] == NULL)
+                        continue;
 
-    // Check for internal shell commands, such as exit
-    if(internal_command(args))
-      continue;
+                // Check for internal shell commands, such as exit
+                if(internal_command(args))
+                        continue;
 
-    // Check for an ampersand
-    block = (ampersand(args) == 0);
+                // Check for an ampersand
+                block = (ampersand(args) == 0);
 
-    // Check for redirected input
-    input = redirect_input(args, &input_filename);
+                // Check for redirected input
+                input = redirect_input(args, &input_filename);
 
-    switch(input) {
-    case -1:
-      printf("Syntax error!\n");
-      continue;
-      break;
-    case 0:
-      break;
-    case 1:
-      printf("Redirecting input from: %s\n", input_filename);
-      break;
-    }
+                switch(input) {
+                case -1:
+                        printf("Syntax error!\n");
+                        continue;
+                        break;
+                case 0:
+                        break;
+                case 1:
+                        printf("Redirecting input from: %s\n", input_filename);
+                        break;
+                }
 
-    // Check for redirected output
-    output = redirect_output(args, &output_filename);
+                // Check for redirected output
+                output = redirect_output(args, &output_filename);
 
-    switch(output) {
-    case -1:
-      printf("Syntax error!\n");
-      continue;
-      break;
-    case 0:
-      break;
-    case 2:
-      printf("Redirecting output, and appending, to: %s\n", output_filename);
-   	  break;  
-    case 1:
-      printf("Redirecting output to: %s\n", output_filename);
-      break;
-    
-    }
+                switch(output) {
+                case -1:
+                        printf("Syntax error!\n");
+                        continue;
+                        break;
+                case 0:
+                        break;
+                case 2:
+                        printf("Redirecting output, and appending, to: %s\n", output_filename);
+                        break;
+                case 1:
+                        printf("Redirecting output to: %s\n", output_filename);
+                        break;
 
-    // Do the command
-    do_command(args, block, 
-	       input, input_filename, 
-	       output, output_filename);
-  }
+                }
+
+                // Do the command
+                do_command(args, block,
+                           input, input_filename,
+                           output, output_filename);
+        }
 }
 
 /*
  * Check for ampersand as the last argument
- 	TODO
+   TODO
  */
 int ampersand(char **args) {
-  int i;
+        int i;
 
-  for(i = 1; args[i] != NULL; i++) ;
+        for(i = 1; args[i] != NULL; i++);
 
-  if(args[i-1][0] == '&') {
-    free(args[i-1]);
-    args[i-1] = NULL;
-    return 1;
-  } else {
-    return 0;
-  }
-  
-  return 0;
+        if(args[i-1][0] == '&') {
+                free(args[i-1]);
+                args[i-1] = NULL;
+                return 1;
+        } else {
+                return 0;
+        }
+
+        return 0;
 }
 
-/* 
+/*
  * Check for internal commands
- * Returns true if there is more to do, false otherwise 
+ * Returns true if there is more to do, false otherwise
  */
 int internal_command(char **args) {
-  if(strcmp(args[0], "exit") == 0) {
-    exit(0);
-  }
+        if(strcmp(args[0], "exit") == 0) {
+                exit(0);
+        }
 
-  return 0;
+        return 0;
 }
 
-/* 
+/*
  * Do the command
  */
 int do_command(char **args, int block,
-	       int input, char *input_filename,
-	       int output, char *output_filename) {
-  
-  int result;
-  pid_t child_id;
-  int status;
+               int input, char *input_filename,
+               int output, char *output_filename) {
 
-  // Fork the child process
-  child_id = fork();
+        int result;
+        pid_t child_id;
+        int status;
 
-  // Check for errors in fork()
-  switch(child_id) {
-  case EAGAIN:
-    perror("Error EAGAIN: ");
-    return;
-  case ENOMEM:
-    perror("Error ENOMEM: ");
-    return;
-  }
+        // Fork the child process
+        child_id = fork();
 
-  if(child_id == 0) {
+        // Check for errors in fork()
+        switch(child_id) {
+        case EAGAIN:
+                perror("Error EAGAIN: ");
+                return;
+        case ENOMEM:
+                perror("Error ENOMEM: ");
+                return;
+        }
 
-    // Set up redirection in the child process
-    if(input)
-      freopen(input_filename, "r", stdin);
+        if(child_id == 0) {
 
-    if(output == 1)
-      freopen(output_filename, "w+", stdout);
+                // Set up redirection in the child process
+                if(input)
+                        freopen(input_filename, "r", stdin);
 
-    if(output == 2)
-      freopen(output_filename, "a+", stdout);
+                if(output == 1)
+                        freopen(output_filename, "w+", stdout);
 
-    // Execute the command
-    result = execvp(args[0], args);
+                if(output == 2)
+                        freopen(output_filename, "a+", stdout);
 
-    exit(-1);
-  }
+                // Execute the command
+                result = execvp(args[0], args);
 
-  // Wait for the child process to complete, if necessary
-  if(block) {
-    printf("Waiting for child, pid = %d\n", child_id);
-    result = waitpid(child_id, &status, 0);
-  }
+                exit(-1);
+        }
+
+        // Wait for the child process to complete, if necessary
+        if(block) {
+                printf("Waiting for child, pid = %d\n", child_id);
+                result = waitpid(child_id, &status, 0);
+        }
 }
 
 /*
  * Check for input redirection
  */
 int redirect_input(char **args, char **input_filename) {
-  int i;
-  int j;
+        int i;
+        int j;
 
-  for(i = 0; args[i] != NULL; i++) {
+        for(i = 0; args[i] != NULL; i++) {
 
-    // Look for the <
-    if(args[i][0] == '<') {
-      free(args[i]);
+                // Look for the <
+                if(args[i][0] == '<') {
+                        free(args[i]);
 
-      // Read the filename
-      if(args[i+1] != NULL) {
-	*input_filename = args[i+1];
-      } else {
-	return -1;
-      }
+                        // Read the filename
+                        if(args[i+1] != NULL) {
+                                *input_filename = args[i+1];
+                        } else {
+                                return -1;
+                        }
 
-      // Adjust the rest of the arguments in the array
-      for(j = i; args[j-1] != NULL; j++) {
-	args[j] = args[j+2];
-      }
+                        // Adjust the rest of the arguments in the array
+                        for(j = i; args[j-1] != NULL; j++) {
+                                args[j] = args[j+2];
+                        }
 
-      return 1;
-    }
-  }
+                        return 1;
+                }
+        }
 
-  return 0;
+        return 0;
 }
 
 /*
  * Check for output redirection
  */
 int redirect_output(char **args, char **output_filename) {
-  int i;
-  int j;
-  int k;
+        int i;
+        int j;
+        int k;
 
-  for(i = 0; args[i] != NULL; i++) {
+        for(i = 0; args[i] != NULL; i++) {
 
-  	// Look for the >
-    if(args[i][0] == '>' && args[i+1][0] == '>') {
-      free(args[i]);
+                // Look for the >
+                if(args[i][0] == '>' && args[i+1][0] == '>') {
+                        free(args[i]);
 
-      // Get the filename 
-      if(args[i+2] != NULL) {
-		*output_filename = args[i+2];
-      } else {
-		return -1;
-      }
+                        // Get the filename
+                        if(args[i+2] != NULL) {
+                                *output_filename = args[i+2];
+                        } else {
+                                return -1;
+                        }
 
-      // Adjust the rest of the arguments in the array
-      for(j = i; args[j-1] != NULL; j++) {
-		args[j] = args[j+2];
-      }
+                        // Adjust the rest of the arguments in the array
+                        for(j = i; args[j-1] != NULL; j++) {
+                                args[j] = args[j+2];
+                        }
 
-      return 2;
-    }
-  
+                        return 2;
+                }
 
-    // Look for the >
-    if(args[i][0] == '>') {
-      free(args[i]);
 
-      // Get the filename 
-      if(args[i+1] != NULL) {
-		*output_filename = args[i+1];
-      } else {
-		return -1;
-      }
+                // Look for the >
+                if(args[i][0] == '>') {
+                        free(args[i]);
 
-      // Adjust the rest of the arguments in the array
-      for(j = i; args[j-1] != NULL; j++) {
-		args[j] = args[j+2];
-      }
+                        // Get the filename
+                        if(args[i+1] != NULL) {
+                                *output_filename = args[i+1];
+                        } else {
+                                return -1;
+                        }
 
-      return 1;
-    }
-  }
+                        // Adjust the rest of the arguments in the array
+                        for(j = i; args[j-1] != NULL; j++) {
+                                args[j] = args[j+2];
+                        }
 
-  return 0;
+                        return 1;
+                }
+        }
+
+        return 0;
 }
-
-
